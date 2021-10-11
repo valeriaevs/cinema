9589fddd2246560556643270ee95439d2c933611
Valeria Evseenkova <evseenkovavaleria2001@mail.ru>
2021-10-11 23:22:59 +0300

    feat: add adaptive and video

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-counter");
 let cost = 800;
 let totalPrice = 0;
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 
 schemeSvg.addEventListener("click", (event) => {
   if (!event.target.classList.contains('booked')) {
@@ -10,6 +12,9 @@ schemeSvg.addEventListener("click", (event) => {
     totalPrice = totalSeats * cost;
     totalPriceTag.textContent = totalPrice;
     console.log();
-  }
-  
-})
+  } 
+});
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('is-open');
+});
